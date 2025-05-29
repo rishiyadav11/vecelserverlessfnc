823bd3e6d02181dd841dac6d20c1da9e84db099b
rishiyadav11 <photonics54yadav@gmail.com>
2025-05-29 14:24:39 +0530

    updated db

--- api/bookings.js
+++ api/bookings.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../lib/db';
+import { connectToDatabase } from '../lib/db.js';
 
 export default async function handler(req, res) {
   const client = await connectToDatabase();
